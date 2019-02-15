39598d292496e275b27489d4f5bbe767218c917e
NAWIF <nnawif@gmail.com>
2019-02-15 16:16:04 +0300

    group schema done

---
+++ database/migrations/2019_02_15_131458_create_group_permissions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroupPermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_permissions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreign('group_id')->references('id')->on('groups'->onDelete('cascade'));
+            $table->foreign('user_id')->references('id')->on('users'->onDelete('cascade'));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group_permissions');
+    }
+}
