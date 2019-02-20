e04b61180f05e2c58fd7b7e0929e02449ea364a8
NAWIF <nnawif@gmail.com>
2019-02-20 20:24:30 +0300

    report page finished, files remaning

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -1,13 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Report;
 
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
     public function getReport($id){
-
-        return view('report.reportPage',['name' => 'hello world']);
+        $report = Report::find($id);
+        $report['author'] = $report->author()->first()->toArray();
+        $report['tags'] = $report->tags->toArray();
+        $report =$report -> toArray();
+        return view('report.reportPage',['report' => $report]);
     }
 }
