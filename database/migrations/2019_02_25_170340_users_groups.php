5461e90d10aea4e35e21c75092ccef76338f1d38
NAWIF <nnawif@gmail.com>
2019-02-25 20:54:23 +0300

    user group many to many pivot table added

--- database/migrations/2019_02_25_170340_users_groups.php
+++ database/migrations/2019_02_25_170340_users_groups.php
@@ -4,26 +4,27 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateGroupPermissionsTable extends Migration
+class UsersGroups extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('group_permissions', function (Blueprint $table) {
-            $table->increments('id');
-            
+    public function up(){
+
+        Schema::create('users_groups', function (Blueprint $table){
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
             $table->integer('group_id')->unsigned();
             $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
 
-            $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
+
         });
     }
+
 
     /**
      * Reverse the migrations.
@@ -32,6 +33,6 @@ class CreateGroupPermissionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('group_permissions');
+        //
     }
 }
