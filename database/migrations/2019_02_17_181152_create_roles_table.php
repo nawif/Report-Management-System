b4c73789a0d806f775a1bb5498da4f741beb4466
NAWIF <nnawif@gmail.com>
2019-02-25 18:51:12 +0300

    roles schema fixed

--- database/migrations/2019_02_17_181152_create_roles_table.php
+++ database/migrations/2019_02_17_181152_create_roles_table.php
@@ -16,8 +16,6 @@ class CreateRolesTable extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('role');
-            $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
